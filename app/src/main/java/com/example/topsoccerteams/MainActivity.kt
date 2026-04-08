176a6bf68e2ff29f00c47a7c56e222f27cd83510
aqeelahhh <ST10516749@myemeris.edu.za>
2026-04-08 09:51:48 +0200

    added while loop to teams array

--- app/src/main/java/com/example/topsoccerteams/MainActivity.kt
+++ app/src/main/java/com/example/topsoccerteams/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.topsoccerteams
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.util.Arrays
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,6 +20,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        //creating a variable to hold TextView Text
+        val teamsTxt= findViewById<TextView>(R.id.TeamDisplay)
+
+        //Reassigned position 0
+        teams [0] = "Memelodi Sundowns FC"
+
+        //putting array in textview
+        teamsTxt.text = Arrays.toString(teams)
+
+        //assigning array content to a variable
+        var teamsDisplay = ""
+        var count = 0
+
+        while (count < teams.count()) {
+            teamsDisplay += "${teams [count]}\n"
+            count ++
+
+        }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
